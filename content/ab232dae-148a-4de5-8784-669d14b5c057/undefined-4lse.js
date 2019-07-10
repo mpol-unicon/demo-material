6330ccaf4b45c9c69d1ab176561d5a23b7cffdca
Mark Pollack <mpollack@unicon.net>
2019-07-09 18:40:40 -0700

    Added content page

---
+++ content/ab232dae-148a-4de5-8784-669d14b5c057/undefined-4lse.js
@@ -0,0 +1,13 @@
+function hideShowTrans(id) {
+    let container = document.getElementById("transcripts");
+    container.classList.toggle("trans-hidden");
+}
+
+//http://nnattawat.github.io/flip/
+
+/*$("#card").flip({
+});*/
+
+$(".flip-card").flip({
+    
+});
